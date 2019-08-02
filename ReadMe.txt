65332f522af70c7bad7e62c23b5178d0140e9428
Syuichi Murashima <mura-github@vwnet.jp>
2019-08-02 11:14:31 +0900

    1st commit

---
+++ ReadMe.txt
@@ -0,0 +1,29 @@
+﻿管理者として実行
+
+■ これは何?
+コマンド(プログラム)を管理者として実行します。
+
+■ 使い方
+sudo コマンド 引数...
+
+■ セットの仕方
+install.ps1 を実行してください
+
+次に PowerShell プロンプト開くと sudo コマンドが使えるようになっています。
+
+■ Uninstall 方法
+uninstall.ps1 を実行して下さい
+(問い合わせが来たら Enter)
+
+■ 動作確認環境
+PowerShell 5.1
+Windows 環境専用です。
+
+■ Web サイト
+管理権限で実行
+https://www.vwnet.jp/Windows/PowerShell/runas.htm
+
+■ リポジトリ
+https://github.com/MuraAtVwnet/sudo
+git@github.com:MuraAtVwnet/sudo.git
+
---
+++ sudo.psm1
@@ -0,0 +1,31 @@
+﻿#########################################
+# 管理者として実行
+#########################################
+function sudo(){
+	if( $args.Count -eq 0 ){
+		echo "Usage...."
+		echo "    sudo コマンド 引数...."
+		echo ""
+		return
+	}
+
+	$ProgramFlag = $True
+	[String]$Argument = ""
+	foreach( $arg in $args ){
+		if( $ProgramFlag ){
+			$Program = $arg
+			$ProgramFlag = $False
+		}
+		else{
+			$Argument += $arg + " "
+		}
+	}
+
+	if( $Argument -eq [String]$null ){
+		Start-Process $Program -Verb runas
+	}
+	else{
+		Start-Process $Program -Verb runas $Argument
+	}
+}
+
